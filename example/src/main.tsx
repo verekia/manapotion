7384e5278c087d643a2c501572a558f8abda0b01
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2024-03-04 00:33:03 +0700

    fix: remove absolute path import from example

--- example/src/main.tsx
+++ example/src/main.tsx
@@ -2,9 +2,9 @@ import { StrictMode } from 'react'
 
 import { createRoot } from 'react-dom/client'
 
-import App from '#/App.tsx'
+import App from './App.tsx'
 
-import '#/index.css'
+import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
