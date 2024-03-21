f9832e734402105ec46211aaadfa66721a66b5dd
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2024-03-21 22:54:07 +0700

    fix: move store re-export to index.ts

--- packages/react/index.ts
+++ packages/react/index.ts
@@ -2,3 +2,5 @@ export * from './hooks'
 export * from './Listeners'
 export * from './store'
 export * from './JoystickArea'
+
+export * from '@manapotion/store'
--- packages/react/store.ts
+++ packages/react/store.ts
@@ -2,8 +2,6 @@ import { useStore } from 'zustand'
 
 import { manaPotionStore, ManaPotionState } from '@manapotion/store'
 
-export * from '@manapotion/store'
-
 export function useMP(): ManaPotionState
 export function useMP<T>(selector: (state: ManaPotionState) => T): T
 export function useMP<T>(selector?: (state: ManaPotionState) => T) {
