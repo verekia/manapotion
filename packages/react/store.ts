3959d552081df921345e347cf04a92f69582eb9f
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2024-03-21 22:46:24 +0700

    fix: re-export all from store

--- packages/react/store.ts
+++ packages/react/store.ts
@@ -2,7 +2,7 @@ import { useStore } from 'zustand'
 
 import { manaPotionStore, ManaPotionState } from '@manapotion/store'
 
-export { mp } from '@manapotion/store'
+export * from '@manapotion/store'
 
 export function useMP(): ManaPotionState
 export function useMP<T>(selector: (state: ManaPotionState) => T): T
