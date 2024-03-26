842b5a5d3bc81bbded53f8d9c9030e98e8627d2f
verekia <hello@verekia.com>
2024-03-26 12:50:58 +0700

    chore: add syncpack, update peer ranges

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,18 +4,7 @@ module.exports = {
     'scope-enum': [
       2,
       'always',
-      [
-        'global',
-        'root',
-        'example',
-        'core',
-        'manapotion',
-        'r3f',
-        'react',
-        'tailwind',
-        'vanilla',
-        'vue',
-      ],
+      ['example', 'core', 'manapotion', 'r3f', 'react', 'tailwind', 'vanilla', 'vue'],
     ],
   },
 }
