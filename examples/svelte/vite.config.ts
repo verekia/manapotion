2ba35c78cf58dc8956b84f1732b6a146b89dbea7
verekia <hello@verekia.com>
2024-03-26 15:52:07 +0700

    chore(example): fix svelte example vite config formatting

--- examples/svelte/vite.config.ts
+++ examples/svelte/vite.config.ts
@@ -1,8 +1,6 @@
 import { svelte } from '@sveltejs/vite-plugin-svelte'
-
 import { defineConfig } from 'vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
 })
