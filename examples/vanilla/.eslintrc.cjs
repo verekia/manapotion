026715020efc1cac7d582d39c300d8f8220e70e8
verekia <522007+verekia@users.noreply.github.com>
2024-03-30 08:46:10 +0700

    chore(example,vanilla,tailwind): add ESLint to all vanilla JS packages

---
+++ examples/vanilla/.eslintrc.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  env: { browser: true, es2021: true },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  overrides: [
+    {
+      env: { node: true },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: { sourceType: 'script' },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'no-extra-semi': 'off',
+  },
+}
